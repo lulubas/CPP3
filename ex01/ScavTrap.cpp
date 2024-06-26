f50d9b03b2a7ec3c2adbd06139093a052c160e41
John Doe <johndoe@email.com>
2024-06-26 13:00:19 +0200

    small work in the airport

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,8 +6,45 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 04:31:56 by lbastien          #+#    #+#             */
-/*   Updated: 2024/06/25 04:32:09 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/06/25 23:46:01 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
+
+std::map<std::string, ClapTrap*> ClapTrap::_clapTrapMap;
+int ClapTrap:: _nameCounter = 1;
+
+ScavTrap::ScavTrap() : ClapTrap::ClapTrap() {
+    std::cout << "ScavTrap " << _name << " created with default constructor." << std::endl;
+}
+
+ScavTrap::ScavTrap( std::string name ) : _name( name ), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+    if (_ScavTrapMap.find( _name ) != _ScavTrapMap.end()) {
+        _name = _generateUniqueName( _name );
+    }
+    _ScavTrapMap[_name] = this;
+    std::cout << "ScavTrap " << _name << " created with parameterized constructor." << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : _name(other._name), _hitPoints(other._hitPoints), _energyPoints(other._energyPoints), _attackDamage(other._attackDamage) {
+    _name = _generateUniqueName( _name );
+    _ScavTrapMap[_name] = this;
+    std::cout << "ScavTrap " << _name << " created with copy constructor." << std::endl;
+}
+
+ScavTrap& ScavTrap::operator=( const ScavTrap &other ) {
+    if (this != &other) {
+        _name = _generateUniqueName( other._name );
+        _hitPoints = other._hitPoints;
+        _energyPoints = other._energyPoints;
+        _attackDamage = other._attackDamage;
+        std::cout << "ScavTrap " << _name << " assigned from another ScavTrap." << std::endl;
+    }
+    return *this;
+}
+
+ScavTrap::~ScavTrap() {
+    _ScavTrapMap.erase( _name );
+    std::cout << "ScavTrap " << _name << " is destructed." << std::endl;
+}
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 04:29:53 by lbastien          #+#    #+#             */
-/*   Updated: 2024/06/25 04:31:26 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/06/25 15:33:01 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 #include "ClapTrap.hpp"
 
-
+class ScavTrap : public ClapTrap {
+        ScavTrap();
+        ScavTrap( std::string name );
+        ScavTrap( const ScavTrap &other );
+        ScavTrap& operator=( const ScavTrap &other );
+        ~ScavTrap();
+}
 
 #endif
